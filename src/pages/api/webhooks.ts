a3f09c65410b243173b698f64f365cb4604a2e7c
diogo.santiago <diogo.santiago@pentagrama.com.br>
2022-02-20 20:40:14 -0300

    iniciando deploy

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -21,7 +21,7 @@ export const config = {
 
 const relevantEvents = new Set([
     'checkout.session.completed',
-    // 'customer.subscription.created',
+    'customer.subscription.created',
     'customer.subscription.updated',
     'customer.subscription.deleted',
 ])
@@ -45,7 +45,7 @@ export default async function Webhooks(req: NextApiRequest, res: NextApiResponse
         if(relevantEvents.has(type)){
             try{
                 switch(type){
-                    // case 'customer.subscription.created':
+                    case 'customer.subscription.created':
                     case 'customer.subscription.updated':
                     case 'customer.subscription.deleted':
                         const subscription = event.data.object as Stripe.Subscription
